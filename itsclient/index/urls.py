3dad7812aa66c5250d036c9e6e4d439b33f58572
Kevin-Sanghoi <kevinsanghoi7@gmail.com>
2017-09-14 07:40:46 +0530

    postgis

--- itsclient/index/urls.py
+++ itsclient/index/urls.py
@@ -1,7 +1,11 @@
 from django.conf.urls import url
-
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.index, name='index'),
+    url(r'^rest', views.index, name='index'),
+    url(r'^home', views.index1, name='index1'),
+    url(r'^technology', views.technology, name='technology'),
+    url(r'^faq', views.faq, name='faq'),
+    url(r'^contactus', views.contactus, name='contactus'),
+    url(r'^landlord/', views.landlord, name='landlord'),
 ]
--- itsclient/index/views.py
+++ itsclient/index/views.py
@@ -7,3 +7,18 @@ from django.http import HttpResponse
 
 def index(request):
 	return render(request, 'index/index.html')
+
+def index1(request):
+	return render(request, 'index/index1.html')
+
+def technology(request):
+	return render(request, 'index/technology.html')
+
+def faq(request):
+	return render(request, 'index/pricing.html')
+
+def contactus(request):
+	return render(request, 'index/contact.html')
+
+def landlord(request):
+	return render(request, 'index/landlords.html')
